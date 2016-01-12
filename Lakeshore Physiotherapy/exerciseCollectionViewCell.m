6fff23bdd6288ea5ca40a4f93c63777a906d32e8
tristan-secord <tristan.secord@hotmail.com>
2016-01-11 21:38:12 -0500

    Cleaning up files

---
+++ Lakeshore Physiotherapy/exerciseCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  exerciseCollectionViewCell.m
+//  Lakeshore Physiotherapy
+//
+//  Created by Tristan Secord on 2014-12-23.
+//  Copyright (c) 2014 Tristan Secord. All rights reserved.
+//
+
+#import "exerciseCollectionViewCell.h"
+
+@implementation exerciseCollectionViewCell
+
+@end
